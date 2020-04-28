708bde5e9e34fbefee2387ca702078cb81e0efdf
gisma_gismo <63441474+Anna-Gisma@users.noreply.github.com>
2020-04-28 22:17:35 +0300

    Update Big_minus.py

--- Big_minus.py
+++ Big_minus.py
@@ -33,13 +33,4 @@ def BigMinus(s1, s2):
     return strk_res
 
 
-import random
-strk = []
-for i in range(10**2):
-    a = str(random.randint(1, 9))
-    strk.append(a)
-strkstr = ''.join(strk)
-s1 = strkstr
-s2 = '1'
-print('str', strkstr)
-print('big', BigMinus(s1, s2))
+
