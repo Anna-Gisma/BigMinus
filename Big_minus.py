283b66314ef91df7fb2d653d47bd554f9f45951c
gisma_gismo <63441474+Anna-Gisma@users.noreply.github.com>
2020-04-29 14:00:11 +0300

    Add files via upload

--- Big_minus.py
+++ Big_minus.py
@@ -12,25 +12,41 @@ def BigMinus(s1, s2):
     if len(r_s1) > len(r_s2):
         big = list(r_s1)
         sml = list(r_s2)
-    else:
+    elif len(r_s1) < len(r_s2) :
         big = list(r_s2)
         sml = list(r_s1)
+    else:
+        if int(s1) > int(s2):
+            big = list(r_s1)
+            sml = list(r_s2)
+        else:
+            big = list(r_s2)
+            sml = list(r_s1)
     
-    for i in range(len(s2)):
-        if r_s1[i] >= r_s2[i]:
-            x = str(int(r_s1[i]) - int(r_s2[i]))
+    for i in range(len(sml)):
+        if big[i] >= sml[i]:
+            x = str(int(big[i]) - int(sml[i]))
             new.append(x)
         else:
-            x = str((int(r_s1[i]) + 10) - int(r_s2[i]))
-            r_s1[i+1] = str(int(r_s1[i+1]) - 1)
+            x = str((int(big[i]) + 10) - int(sml[i]))
+            big[i+1] = str(int(big[i+1]) - 1)
             new.append(x)
     for i in range(len(new)):
-        r_s1[i] = new[i]
-    for i in reversed(r_s1):
+        big[i] = new[i]
+    for i in reversed(big):
         res.append(i)
     strk_res = ''.join(res)
     
     return strk_res
 
 
-
+import random
+strk = []
+for i in range(10**2):
+    a = str(random.randint(1, 9))
+    strk.append(a)
+strkstr = ''.join(strk)
+s1 = '978'
+s2 = '12345'
+#print('str', strkstr)
+print('big', BigMinus(s1, s2))
